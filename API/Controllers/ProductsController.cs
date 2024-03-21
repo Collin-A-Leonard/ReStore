e23ba15aeb0da035544b37e7b93a2072174234a5
Collin-A-Leonard <collin.a.leonard@gmail.com>
2024-03-21 11:58:20 -0400

    End Of Section 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -22,7 +22,7 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/products/3
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            Product product = await _context.Products.FindAsync(id);
 
             if (product == null) return NotFound();
 
