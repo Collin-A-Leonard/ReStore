c2b32fd9903819585851e4970451509fb6ec3a90
Collin-A-Leonard <collin.a.leonard@gmail.com>
2024-04-01 15:13:58 -0400

    End Of Section 9

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,49 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
+        public TokenService(UserManager<User> userManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _config = config;
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+            foreach (string role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSettings:TokenKey"]));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            JwtSecurityToken tokenOptions = new JwtSecurityToken
+            (
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+    }
+}
